4a4f9a3426c045eab581b9b8d6dc516f42f13b6a
anshuul <anshultated1999@gmail.com>
2021-05-13 01:52:33 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "./uploads/")));
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 }
